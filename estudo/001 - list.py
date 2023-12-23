263be140f1ee3a7e22cbb24aa73c80bb91b65b0b
marcusmartins07 <marvyn0710@gmail.com>
2023-12-23 09:52:24 -0300

    #

---
+++ estudo/001 - list.py
@@ -0,0 +1,19 @@
+#converte string em lista
+
+lista = list('Uma frase digitada')
+
+#convertendo uma lista em string
+
+lista2 = ['mais', 'uma', 'frase', 'digitada']
+print(lista2)
+
+# coloca espaço entre cada elemento e transforma e string
+frase = ' '.join(lista2)
+print(frase)
+
+# Coloca um cifrão em cada elemento e transforma em uma string
+frase = '$'.join(lista2)
+print(frase)
+
+
+
