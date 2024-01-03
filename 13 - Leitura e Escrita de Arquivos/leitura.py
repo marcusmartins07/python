08f78137aba0d1a0ace19abfa795f621e1e73c37
marcusmartins07 <marvyn0710@gmail.com>
2024-01-03 09:39:29 -0300

    #

---
+++ 13 - Leitura e Escrita de Arquivos/leitura.py
@@ -0,0 +1,12 @@
+# open(caminho ao caminho para ser lido)
+
+# retorna um _io.TextIOWrapper 
+
+arquivo = open('C://Users/Marcus Martins/Documents/Estudos/python publico/python/13 - Leitura e Escrita de Arquivos/texto.txt')
+print(arquivo)
+
+#print(arquivo.read())
+
+ret = arquivo.read()
+
+print(ret)
