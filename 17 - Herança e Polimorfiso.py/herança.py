94d5b1e336b90aa52ef5086214a7ed47aa6f5e89
marcusmartins07 <marvyn0710@gmail.com>
2024-01-13 09:44:03 -0300

    #

---
+++ 17 - Herança e Polimorfiso.py/herança.py
@@ -0,0 +1,8 @@
+"""
+POO - Herança
+
+A ideia de herança é de reaproveitar o código
+
+A partir de uma classe existente, nós extendemos outra classe que passa a herdar atributos e métodos da classe herdada 
+
+"""
